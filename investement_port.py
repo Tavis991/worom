7e7edad42ade02e27f3a6d1de31a9442b70ba6d2
Tavis991 <frostymd82@gmail.com>
2022-09-20 23:59:20 +0300

    end of day
    Signed-off-by: Tavis991 <frostymd82@gmail.com>

--- investement_port.py
+++ investement_port.py
@@ -22,17 +22,20 @@ DIM = 20
 
 class hypervol_solver():
     def __init__(self, funcs, covariance, wealth, returns):
-        self.covariance = covariance
-        self.returns = returns
+        self.covariance = np.array(covariance)
+        self.returns = np.array(returns)
         self.wealth = wealth
         self.counter = 0
         if len(funcs) > 3 or len(funcs) < 1 : 
             raise Exception('1-3 objective functions per solver')
         self.funcs = funcs
         k = np.random.rand(500,20) #20 investment
-        k3 = (k / sum(k)) * wealth #100 is budget
+        k3 = self.normalize(k) # normalizing 
         self.x_vec = k3
+        # for i in range (500) : 
+        #     self.x_vec[i][np.random.randint(0,19)] += (100 - sum(k3)[i]) # remainder 
         self.y_vec = self.evaluation(self.x_vec)
+
         # evaluated_f1_sp = self.funcs[0](samples[:,0],samples[:,1]) #
         # for f in funcs : 
             
@@ -41,35 +44,40 @@ class hypervol_solver():
         # res = np.stack((evaluated_f1_sp, evaluated_f2_sp), axis=1)
     
     def evaluation (self, x_vec):
-        y_vec = np.array([[self.funcs[i](x_vec[j]) for j in range \
+        info = [self.covariance, self.returns] 
+        y_vec = np.array([[self.funcs[i](x_vec[j], info[i]) for j in range \
              (x_vec.shape[0])] for i in range (len(self.funcs))] ).T
         return y_vec
 
     def build_front(self):
         y = self.pareto_ranking(self.y_vec)
-        front_size = len(y) - np.count_nonzero(y)   
-        arg_v = np.argsort(y)[:front_size] #min(front_size, MIU_SIZE)
+        self.front_size = len(y) - np.count_nonzero(y)   
+        arg_v = np.argsort(y)[:self.front_size] #min(front_size, MIU_SIZE)
 
         miu_y = self.y_vec[arg_v]  
         miu_x = self.x_vec[arg_v]
         
         # MIU_SIZE #to prevent population explosion  #DOES NOT TAKE INTO ACCCOUNT COVERAGE AREA HERE
-        if front_size > MIU_SIZE:
-            sorted_miu = np.linalg.norm(miu_y, axis=1)
-            min_inde = np.argsort(sorted_miu)[:MIU_SIZE] 
-            miu_y = miu_y[min_inde]
-            miu_x = miu_x[min_inde]
-            
-        
+        # if front_size > MIU_SIZE:
+        #     sorted_miu = np.linalg.norm(miu_y, axis=1)
+        #     min_inde = np.argsort(sorted_miu)[:MIU_SIZE] 
+        #     miu_y = miu_y[min_inde]
+        #     miu_x = miu_x[min_inde]
 
         self.front = [np.zeros((miu_x.shape)), np.zeros((miu_y.shape))]
         self.front[0] = np.copy(miu_x)
         self.front[1] = np.copy(miu_y)
-        
+    
+    def normalize(self, vec):
+        zi = (vec / (np.sum (vec, axis=1)).reshape(len(vec), 1)) * self.wealth
+        for i in range (len(zi)) : 
+           zi[i][np.random.randint(0, len(returns)-1)] += (100 - np.sum(zi, axis=1)[i])
+        return zi 
+
     def iterate(self): 
         while self.front[0].shape[0] < 100 :
             for pair in list (itr.combinations(self.front[0], 2)) : #to prevent population extinction 
-                self.front[0] = np.append(self.front[0], self.recombine(pair[0],pair[1]).reshape(1,80), axis=0)
+                self.front[0] = np.append(self.front[0], self.normalize(self.recombine(pair[0],pair[1]).reshape(1,20)), axis=0)
 
         self.y_vec = self.evaluation(self.front[0])
         self.x_vec = self.front[0]
@@ -103,17 +111,18 @@ class hypervol_solver():
         plt.show()
         
         dim = reference_point.shape[0]#volume to be filled 
-        front_by_y = np.copy(self.front[1])
+        #front_by_y = np.copy(self.front[1])
       
         if dim == 2 :
-    
-            idexs = front_by_y[:, 1].argsort()
-            vols_by_y = front_by_y[idexs]
-            differences = np.diff(vols_by_y,axis = 0) #CAN GET RID OF EDGE HERE I THINK
-            contributions = np.abs(differences[:-1, 0] * differences[1:, 1]) #ןIS THIS RIGHT??
-            self.front[1] = np.delete(self.front[1], idexs[np.argmin(contributions)+1], axis=0)
-            self.front[0] = np.delete(self.front[0], idexs[np.argmin(contributions)+1], axis=0)
-            
+            while self.front_size > MIU_SIZE:
+                front_by_y = np.copy(self.front[1])
+                idexs = front_by_y[:, 1].argsort()
+                vols_by_y = front_by_y[idexs]
+                differences = np.diff(vols_by_y,axis = 0) #CAN GET RID OF EDGE HERE I THINK
+                contributions = np.abs(differences[:-1, 0] * differences[1:, 1]) #ןIS THIS RIGHT??
+                self.front[1] = np.delete(self.front[1], idexs[np.argmin(contributions)+1], axis=0)
+                self.front[0] = np.delete(self.front[0], idexs[np.argmin(contributions)+1], axis=0)
+                self.front_size -= 1
             #updating volume without lsp # technically only need to update two, but..
             front_y_2 = np.copy(self.front[1])
             idexs_2 = front_y_2[:, 1].argsort()
@@ -123,12 +132,6 @@ class hypervol_solver():
             contributions_2 = np.abs(differences_2[:-1, 0] * differences_2[1:, 1]) #NEED TO UPDATE DIFFFERNCES FOR THIS TO DO SOMETHING
 
         return sum(contributions_2)
-        # ax1.scatter(self.front[1][idexs[np.argmin(contributions)+1]][0], self.front[1][idexs[np.argmin(contributions)+1]][1], color='grey')
-        # vols_inclus = []    
-        # for p in front_by_y :
-        #     vols_inclus.append(self.inclusive(reference_point, p))
-        # return (sum(vols_inclus))    
-        #TODO how to detecet overlap 
 
     def inclusive(self, refpoint, point): #what does this do??
         """
@@ -158,15 +161,21 @@ class hypervol_solver():
         for m in range(dim) :
             dystopia[m] = np.max(miu[:,m]) #CHANGED MIU TO Y_VEC
         return dystopia #DO I NEED TO MAKE A COPY OF Y_VEC?
+    def dominate(self, x1: np.array, x2: np.array):
+        return (x1 <= x2).all() and (x1 < x2).any()
 
-    def pareto_ranking(self, ObjFncVectors) :  #any dimensional
-        ranking = np.zeros(len(ObjFncVectors)) #WHY NOT JUST SELF.Y_VEC.shape[0]?? ""^
-        for idx in range(len(ObjFncVectors)): #dominating vec 
-            for idx2 in range(len(ObjFncVectors)): #dominated vec 
-                if np.all(ObjFncVectors[idx,:] <= ObjFncVectors[idx2,:]) \
-                    and np.any(ObjFncVectors[idx,:] < ObjFncVectors[idx2,:] ) :
-                    ranking[idx2] += 1 
-        return ranking
+    def pareto_ranking(self, ObjFncVectors):
+        X = pd.DataFrame(ObjFncVectors)
+        return np.array([np.sum(X.T.apply(lambda x: self.dominate(x, X.T[i]), axis=0)) for i in range(X.shape[0])])
+    # def pareto_ranking(self, ObjFncVectors) :  #any dimensional
+    #     ranking = np.zeros(len(ObjFncVectors)) #WHY NOT JUST SELF.Y_VEC.shape[0]?? ""^
+
+    #     for idx in range(len(ObjFncVectors)): #dominating vec 
+    #         for idx2 in range(len(ObjFncVectors)): #dominated vec 
+    #             if np.all(ObjFncVectors[idx, :] <= ObjFncVectors[idx2, :]) \
+    #                 and np.any(ObjFncVectors[idx, :] < ObjFncVectors[idx2, :] ) :
+    #                 ranking[idx2] += 1 
+    #     return ranking
 
     def best_point(self):
         y = self.front[1][np.argmin(np.linalg.norm(self.front[1], axis=1))]
@@ -189,17 +198,17 @@ f3_1 = lambda x1 : np.dot(x1.T , x1) # minimize
 f3_2 = lambda x1 : np.dot((x1-1).T, x1-1) # minimize
 f3_3 = lambda x1 : np.dot((x1-2).T, x1-2) # minimize
 
-import csv
-#datContent = [i.strip().split() for i in open("portfolio.dat.txt").readlines()]
 cov_parse = lambda row : [float(i) for i in row[2:-3].split(',')]
 
 df = pd.read_table('portfolio.dat') # ['Investments' , 'Return' , 'Covariance' , 'Wealth']
-funcs = [f2_risk, neg_f2_return]
-solver = hypervol_solver(funcs)
-front = solver.solve()
 returns = [float(i) for i in df.values[7][0].split('=')[1].split(';')[0][2:-2].split(',')] 
-covariances = [cov_parse(df.values[9+i][0]) for i in range(20)]
-wealth = df.values[30][0].split('=')[1].split(';')[0]
+covariance = [cov_parse(df.values[9+i][0]) for i in range(20)]
+wealth = int(df.values[30][0].split('=')[1].split(';')[0])
+funcs = [f2_risk, neg_f2_return]
+
+solver = hypervol_solver(funcs, covariance, wealth, returns)
+front = solver.solve()
+
 
 print(front[1][np.argmin(np.linalg.norm(front[1], axis=1))])
 
